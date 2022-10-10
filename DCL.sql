160f06ad4ee24cc2e8018e5ea950baed741dfbaa
Shofwans <72340199+Shofwans@users.noreply.github.com>
2022-10-10 17:37:15 +0800

    Delete DCL.sql

--- DCL.sql
+++
@@ -1,9 +0,0 @@
-CREATE USER 'DosenSisfor'@'localhost' IDENTIFIED BY '12345';
-
-GRANT ALL PRIVILEGES 
-ON db_praktisiSisfo
-TO 'DosenSisfor'@'localhost';
-
-GRANT SELECT 
-ON db_praktisiSisfo
-TO '*'@'localhost';
